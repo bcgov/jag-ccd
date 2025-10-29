60fda21cf250c39579709d0f663da8f2d1d561e5
Kyle Flood <kyle.flood@gov.bc.ca>
2025-10-29 16:09:21 -0700

    CCD-1455
    Added AgencyEmail GET endpoint to retrieve emails for specific agencies.

---
+++ jag-ccd-application/src/main/java/ca/bc/gov/open/ccd/controllers/AgencyController.java
@@ -0,0 +1,112 @@
+package ca.bc.gov.open.ccd.controllers;
+
+import ca.bc.gov.open.ccd.common.agency.emails.*;
+import ca.bc.gov.open.ccd.exceptions.ORDSException;
+import ca.bc.gov.open.ccd.models.OrdsErrorLog;
+import ca.bc.gov.open.ccd.models.RequestSuccessLog;
+import ca.bc.gov.open.ccd.models.serializers.InstantSerializer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.ws.context.MessageContext;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.soap.SoapHeader;
+import org.springframework.ws.soap.saaj.SaajSoapMessage;
+
+@Slf4j
+@Endpoint
+public class AgencyController {
+    @Value("${ccd.host}" + "common/")
+    private String host = "https://127.0.0.1/";
+
+    @Value("${ccd.generic-agen-id}")
+    private String genericAgenId;
+
+    @Value("${ccd.generic-part-id}")
+    private String genericPartId;
+
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public AgencyController(RestTemplate restTemplate, ObjectMapper objectMapper) {
+        this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    @PayloadRoot(
+            namespace = "http://brooks/CCD.Source.AgencyEmails.ws.provider:AgencyEmails",
+            localPart = "getAgencyEmails")
+    @ResponsePayload
+    public GetAgencyEmailsResponse getAgencyEmails(
+            @RequestPayload GetAgencyEmails getAgencyEmails, MessageContext messageContext)
+            throws JsonProcessingException {
+
+        if (messageContext != null) {
+            SaajSoapMessage soapResponse = (SaajSoapMessage) messageContext.getResponse();
+            soapResponse
+                    .getEnvelope()
+                    .addNamespaceDeclaration(
+                            "SOAP-ENC", "http://schemas.xmlsoap.org/soap/encoding/");
+            soapResponse
+                    .getEnvelope()
+                    .addNamespaceDeclaration("xsd", "http://www.w3.org/2001/XMLSchema");
+            soapResponse
+                    .getEnvelope()
+                    .addNamespaceDeclaration("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+
+            SoapHeader respheader = soapResponse.getSoapHeader();
+
+            respheader.addNamespaceDeclaration(
+                    "SOAP-ENV", "http://schemas.xmlsoap.org/soap/envelope/");
+            respheader.addNamespaceDeclaration(
+                    "SOAP-ENC", "http://schemas.xmlsoap.org/soap/envelope/");
+            respheader.addNamespaceDeclaration(
+                    "SOAP-ENV", "http://schemas.xmlsoap.org/soap/envelope/");
+        }
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(host + "agencyemail");
+        if (getAgencyEmails.getAgenId() != null && !getAgencyEmails.getAgenId().isEmpty()) {
+            builder.queryParam(
+                    "agenId",
+                    getAgencyEmails.getAgenId());
+        }
+        if (getAgencyEmails.getAgenCd() != null && !getAgencyEmails.getAgenCd().isEmpty()) {
+            builder.queryParam(
+                    "agenCd",
+                    getAgencyEmails.getAgenCd());
+        }
+
+        try {
+            HttpEntity<GetAgencyEmailsResponse> resp =
+                    restTemplate.exchange(
+                            builder.build().toUri(),
+                            HttpMethod.GET,
+                            new HttpEntity<>(new HttpHeaders()),
+                            GetAgencyEmailsResponse.class);
+            log.info(
+                    objectMapper.writeValueAsString(
+                            new RequestSuccessLog("Request Success", "getAgencyEmails")));
+
+            return resp.getBody();
+        } catch (Exception ex) {
+            log.error(
+                    objectMapper.writeValueAsString(
+                            new OrdsErrorLog(
+                                    "Error received from ORDS",
+                                    "getAgencyEmails",
+                                    ex.getMessage(),
+                                    getAgencyEmails)));
+            throw new ORDSException();
+        }
+    }
+}
